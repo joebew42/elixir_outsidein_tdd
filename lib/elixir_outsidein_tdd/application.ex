02dce56ce8c66c437f5d1a15b66ff3d2f117a1ff
JoeBew42 <joebew42@gmail.com>
2019-12-02 18:00:28 +0100

    setting up for the meetup

--- lib/elixir_outsidein_tdd/application.ex
+++ lib/elixir_outsidein_tdd/application.ex
@@ -1,39 +1,8 @@
 defmodule ElixirOutsideinTdd.Application do
   use Application
 
-  @opts [
-    greeting_service: GreetingService,
-    messages_service: RandomMessagesService,
-    hour_of_the_day_service: nil
-  ]
-
-  def start(type, []) do
-    start(type, @opts)
-  end
-
-  def start(type, hour_of_the_day_service: hour_of_the_day_service) do
-    start(type, Keyword.merge(@opts, hour_of_the_day_service: hour_of_the_day_service))
-  end
-
-  def start(_type,
-        greeting_service: greeting_service,
-        messages_service: messages_service,
-        hour_of_the_day_service: hour_of_the_day_service
-      ) do
-    children = [
-      Plug.Cowboy.child_spec(
-        scheme: :http,
-        plug: {
-          GreetingServiceRouter,
-          [
-            greeting_service: greeting_service,
-            messages_service: messages_service,
-            hour_of_the_day_service: hour_of_the_day_service
-          ]
-        },
-        options: [port: 4000]
-      )
-    ]
+  def start(_type, _opts) do
+    children = []
 
     opts = [strategy: :one_for_one, name: ElixirOutsideinTdd.Supervisor]
     Supervisor.start_link(children, opts)
