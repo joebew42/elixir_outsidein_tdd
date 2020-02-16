77ee7e3bf5696f4b7b0512e47d4233cfde571b0a
JoeBew42 <joebew42@gmail.com>
2020-02-16 13:20:14 +0100

    greet endpoint will respond with Hello my friend!

---
+++ test/elixir_outsidein_tdd/greeting_features_test.exs
@@ -0,0 +1,15 @@
+defmodule GreetingFeaturesTest do
+  use ExUnit.Case
+
+  describe "When a greeting message is requested" do
+    test "Then the system will reply with 'Hello my friend!'" do
+      ElixirOutsideinTdd.Application.start(nil, [])
+
+      response = HTTPoison.get!("http://localhost:4000/greet")
+
+      assert response.status_code == 200
+      assert response.body == "Hello my friend!"
+    end
+  end
+
+end
