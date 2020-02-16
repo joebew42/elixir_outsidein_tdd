97d9db3b6df67cedf846b0166fe91858826db713
JoeBew42 <joebew42@gmail.com>
2020-02-16 14:11:32 +0100

    do not describe the domain logic in the web tests

--- lib/elixir_outsidein_tdd/application.ex
+++ lib/elixir_outsidein_tdd/application.ex
@@ -3,7 +3,14 @@ defmodule ElixirOutsideinTdd.Application do
 
   def start(_type, _args) do
     children = [
-      Plug.Cowboy.child_spec(scheme: :http, plug: GreetingWeb, options: [port: 4000])
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: {
+          GreetingWeb,
+          greeting: Greeting
+        },
+        options: [port: 4000]
+      )
     ]
 
     opts = [strategy: :one_for_one, name: ElixirOutsideinTdd.Supervisor]
